5abdb8285658599b6bdcfb34acc56673e9899876
Dimaka <70847277+dimakpa@users.noreply.github.com>
2022-04-12 21:14:02 +0300

    Update boundary.js

--- js/boundary.js
+++ js/boundary.js
@@ -1,3 +1,4 @@
+
 function Boundary(x, y, w, h) {
     var options = {
         isStatic: true
@@ -11,6 +12,31 @@ function Boundary(x, y, w, h) {
 
 Boundary.prototype.show = function() {
     fill(160, 11, 35);
+    stroke(0, 10, 75);
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rectMode(CENTER);
+    rect(0, 0, this.w, this.h);
+    pop();
+};
+
+
+function Box(x, y, w, h, color_box, price) {
+    var options = {
+        isStatic: true
+    };
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.body.label = 'box';
+    this.body.price = price;
+    this.w = w;
+    this.h = h;
+    this.color_box = color_box;
+    World.add(world, this.body);
+}
+
+Box.prototype.show = function() {
+    fill(this.color_box);
     stroke(0, 10, 75);
     var pos = this.body.position;
     push();
