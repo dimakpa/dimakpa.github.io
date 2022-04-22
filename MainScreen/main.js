cfb5dfe62089a1ae431dcb81ac6dce31ddfbd2dc
Vladimir1539 <58554800+Vladimir1539@users.noreply.github.com>
2022-04-22 16:31:51 +0300

    Delete MainScreen directory

--- MainScreen/main.js
+++
@@ -1,8 +0,0 @@
-var a = 'https://ru.stackoverflow.com/questions/413328/Как-подключить-файл-js-к-другому-файлу-js'; //ссылка должна быть в ковычках, так как это строка
-
-
-function start() {
-
-    window.open(a, '_self').focus();
-}
-
