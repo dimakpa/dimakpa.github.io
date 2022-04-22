f0ecf293d4f31478cdba48efc9d6ab39261672c7
Vladimir1539 <58554800+Vladimir1539@users.noreply.github.com>
2022-04-22 16:14:29 +0300

    Update main.js

--- MainScreen/main.js
+++ MainScreen/main.js
@@ -1,4 +1,4 @@
-var a = 'http://dimakpa.github.io/koleso/'; //ссылка должна быть в ковычках, так как это строка
+var a = 'http://dimakpa.github.io/'; //ссылка должна быть в ковычках, так как это строка
 
 
 function start() {
