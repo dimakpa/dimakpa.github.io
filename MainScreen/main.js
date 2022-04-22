d20f175b599b94d155933bd0f7d57dddf4b5d82e
Vladimir1539 <58554800+Vladimir1539@users.noreply.github.com>
2022-04-22 23:41:12 +0300

    Delete MainScreen directory

--- MainScreen/main.js
+++
@@ -1,8 +0,0 @@
-
-function Wheel() {
-    window.open('http://dimakpa.github.io/koleso/', '_self').focus();
-}
-
-function Plinko() {
-    window.open('http://dimakpa.github.io/', '_self').focus();
-}
