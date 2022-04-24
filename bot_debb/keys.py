3e39db12ff22932526e7435d43d710a9c1eab027
Dimaka <70847277+dimakpa@users.noreply.github.com>
2022-04-24 14:47:11 +0300

    Add files via upload

---
+++ bot_debb/keys.py
@@ -0,0 +1,7 @@
+api_id   = '1911521'
+api_hash ='8a552b27e4486e58aba3166ccda35e1b'
+username ='debocherry'
+# debocherry
+# token = '1356430120:AAENZ3Hgd1-8h76u8ldnD5cOgu6WxbY0fjM'
+# vodkakasino
+token = '5340330245:AAFXGFFlJ-72WvzJrfa-bKF-hA3kNnQC0Jo'
